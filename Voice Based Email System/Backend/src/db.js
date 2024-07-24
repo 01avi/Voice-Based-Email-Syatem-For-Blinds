89ec9edf627f87c0321e9f4b89f9253fa8b1ecab
Aviral <110219413+01avi@users.noreply.github.com>
2024-07-24 16:11:26 +0530

    Update db.js

--- Voice Based Email System/Backend/src/db.js
+++ Voice Based Email System/Backend/src/db.js
@@ -5,6 +5,6 @@ exports.pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'email_account',
-    password: 'Aviral%12345676',
+    password: '12345676',
     port: 5432,
 })
